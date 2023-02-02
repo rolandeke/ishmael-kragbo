05d909e0abe8005647a28c632ac6b7203a6533d0
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-02-02 12:27:21 +0000

    react app setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="text-red-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        nostrum dolorem modi odit hic commodi totam. Magnam vero dolor
+        recusandae placeat sit sequi, voluptate nihil quae deserunt dolores
+        nulla? Asperiores.
+      </div>
+    </div>
+  );
+}
+
+export default App;
